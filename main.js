b9fc4c8e55b4b1432cd6e7b10fde6227374149f1
Gajiabraham <126649868+Gajiabraham@users.noreply.github.com>
2023-03-23 05:18:04 -0700

    variables, data-types, string-concatenation, string-properties, arrays.

---
+++ main.js
@@ -0,0 +1,49 @@
+//---------------------variables-----------------//
+/*
+let age = 30;
+age = 31;
+console.log(age);
+let score;
+score = 10;
+console.log(score)*/
+//-----------------------Data types-------------//
+/*const name = 'Gaji';
+const age = 30;
+const isCool = true;
+const rating = 4.5;
+const x = null;
+const y = undefined;
+let z;
+console.log(typeof isCool);*/
+//---------------------------String Concatenation-----------//
+/*
+const name = 'Gaji';
+const age = 30;
+console.log('My name is' + name + 'and i am' + age);
+//template string
+console.log('My name is ${name} and i am ${age}');
+const hello = 'My name is ${name} and i am ${age}';
+console.log(hello);*/
+//-------------------string properties/methods------------//
+/*
+const g = 'tech,comp,game';
+const s = 'hello world';
+console.log(s.substring(0, 5).toUpperCase());
+console.log(s.split(''))//spliting content
+console.log(g.split(','))*/
+//-------------------------Arrays-----------------------//
+/*
+const fruits = ['apples', 'oranges', 'pear', 'pineapple'];
+console.log(fruits)
+console.log(fruits[0]);
+fruits[4] = 'grapes'
+console.log(fruits);
+fruits.push('melon');//appropriete method for adding items in a list to the end
+console.log(fruits)
+fruits.unshift('berry'); //''----------------''-----------''
+console.log(fruits)
+fruits.pop();
+console.log(fruits)
+console.log(Array.isArray(fruits))
+console.log(fruits.indexOf('melon'))*/
+//------------------------Object-literal----------------//
